7fb5475415b50fb7fa26e6f403fccfc73964e089
LaraLaPierre <larablapierre@gmail.com>
2018-07-23 11:10:32 -0500

    ripped out table bootstrap, index action working, no table

--- quoteboard/src/components/QuotesContainer.js
+++ quoteboard/src/components/QuotesContainer.js
@@ -1,68 +1,35 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
-import BootstrapTable from 'react-bootstrap-table-next';
-import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
-import paginationFactory from 'react-bootstrap-table2-paginator';
+import Quote from './quote'
 
 class QuotesContainer extends Component {
-  state = {
-    quotes: [],
-    columns: [{
-      dataField: 'id',
-      text: 'ID',
-      sort: true
-    },
-    {
-      dataField: 'context',
-      text: 'Context',
-      sort: true,
-      filter: textFilter()
-    }, 
-    {
-      dataField: 'quote',
-      text: 'Quote',
-      sort: true,
-      filter: textFilter()
-    },
-    {
-      dataField: 'source',
-      text: 'Source',
-      sort: true,
-      filter: textFilter()
-    },
-    {
-      dataField: 'theme',
-      text: 'Theme',
-      sort: true,
-      filter: textFilter()
-    }]
+  constructor(props) {
+    super(props)
+    this.state = {
+      quotes: []
+    }
   }
 
   componentDidMount() {
     axios.get('http://localhost:3001/api/v1/quotes')
-      .then(response => {
-        this.setState({
-          quotes: response.data
-        });
-      });
-  }
-  
+    .then(response => {
+      console.log(response)
+      this.setState({quotes: response.data})
+    })
+    .catch(error => console.log(error))
+  } 
+
   render() {
     return (
-      <div className="container" style={{ marginTop: 50 }}>
-        <BootstrapTable 
-        striped
-        hover
-        keyField='id' 
-        data={ this.state.quotes } 
-        columns={ this.state.columns } 
-        filter={ filterFactory() } 
-        pagination={ paginationFactory() }
-        headerClasses={"header"} />
+      <div>
+        {this.state.quotes.map((quote) => {
+          return (
+            <Quote quote={quote} key={quote.id} />
+          )
+        })}
       </div>
     );
   }
-}
-
+} 
+ 
 export default QuotesContainer
